53102c7ba44190e57689cdab4496cc0bbaa33ddf
Samuel Mensah <samuelnmensah@live.com>
2018-01-03 11:55:44 -0800

    finished exercise

--- list_overlap.py
+++ list_overlap.py
@@ -12,3 +12,21 @@ Randomly generate two lists to test this
 Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 
 """
+# solution 1
+random_list = list(range(1, 10, 2))
+random_list_two = list(range(1, 10, 1))
+
+common_elements = []
+
+for i in random_list_two:
+    if i in random_list:
+        common_elements.append(i)
+
+print(common_elements)
+
+
+# solution 2
+a = list(range(1, 10, 2))
+b = list(range(1, 100, 1))
+c = [x for x in set(a) if x in set(b)]
+print(c)
