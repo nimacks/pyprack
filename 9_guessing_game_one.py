1add4b5af030ac876e6f35809c416ea53009f495
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2018-02-19 09:38:05 -0800

    completed excercise

--- 9_guessing_game_one.py
+++ 9_guessing_game_one.py
@@ -8,3 +8,48 @@ Extras:
 Keep the game going until the user types “exit”
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 """
+
+import random
+import sys
+
+
+def guess_number(user_guess, rand_number, tries):
+    """
+    Method determines if the user_guess matches the rand_number
+    :param user_guess:
+    :param rand_number:
+    :param tries
+    :return: basestring
+    """
+
+    if user_guess < rand_number:
+        return "Too low!"
+    if user_guess > rand_number:
+        return "Too high!"
+    if user_guess == rand_number:
+        return "You guessed correctly and it took you " + str(tries) + " tries"
+    else:
+        sys.exit()
+
+
+def get_random_number(start, end):
+    """
+    Gets a random number between a range of numbers
+    :rtype: int
+    """
+    return random.randint(start, end)
+
+
+random_number = get_random_number(1, 9)
+count = 0
+guess = 0
+
+while guess != random_number and guess != 'q':
+    guess = int(input('Guess a number between 1 and 9 '))
+
+    if guess == 'q':
+        break
+
+    count += 1
+    print(guess_number(guess, random_number, count))
+    print()
