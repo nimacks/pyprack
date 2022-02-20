95886833569828b995b81458a11e1e85a908c43d
Sam <4107271+nimacks@users.noreply.github.com>
2022-02-19 20:59:51 -0800

    (chore) adding longest tweet

---
+++ python_cookbook/1_3_keeping_the_nth_items.py
@@ -0,0 +1,22 @@
+"""
+you want to keep a limited history of the last few items seen during ieration or during some other kind of processing
+"""
+
+from collections import deque
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+        
+def run():
+    with open('/Users/nimacks/Projects/repos/github/pyprack/python_cookbook/1_2_unpacking_elements_arbitrary_length.py') as f:
+        for line,prevlines in search(f.readlines(),'uname',5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-'*20)    
+        
+run()
---
+++ python_cookbook/1_4_largest_of_n.py
@@ -0,0 +1,41 @@
+import heapq
+"""[A program to find the largest or small of n numbers]
+
+"""
+
+
+def find():
+    nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+    small_numbers = heapq.nsmallest(5, nums)
+    large_numbers = heapq.nlargest(3, nums)
+    small_numbers.sort()
+    large_numbers.sort(reverse=True)
+    print(small_numbers)
+    print(large_numbers)
+
+
+def find_tuple():
+    portfolio = [
+        {'name': 'IBM', 'shares': 100, 'price': 91.1},
+        {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+        {'name': 'FB', 'shares': 200, 'price': 21.09},
+        {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+        {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+        {'name': 'ACME', 'shares': 75, 'price': 115.65}
+    ]
+
+    tweets = [
+        {'tweet1': 'welcome to twitter', 'length': 10},
+        {'tweet1': 'this is the second message', 'length': 20}
+    ]
+    
+    longest_tweet = heapq.nlargest(1,tweets,key=lambda k:k['length'])
+    cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['shares'])
+    expensive = heapq.nlargest(1, portfolio, key=lambda p: p['price'])
+    print(cheap)
+    print(expensive)
+    print(longest_tweet)
+
+
+# find()
+find_tuple()
